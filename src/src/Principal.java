c4e86033eda316b1cf103bc6911ba7f6a3d8b9b7
Pablo <pabloms.94@usal.es>
2016-05-18 13:38:29 +0200

    Corregido eje X

--- src/src/Principal.java
+++ src/src/Principal.java
@@ -8,7 +8,9 @@ import javax.swing.JFrame;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -16,21 +18,21 @@ import com.google.gson.JsonParser;
 import com.google.gson.JsonPrimitive;
 
 public class Principal {
-	
-	static DefaultCategoryDataset dataset1 = new DefaultCategoryDataset();
-	static double []x = new double[60];
+	static XYSeriesCollection collection = new XYSeriesCollection();
+    static XYSeries series = new XYSeries("");
+    static XYDataset dataset1;
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		JsonParser parser = new JsonParser();
 		FileReader fr = new FileReader("../../C/integralesV2/prueba1.json");
 		JsonElement datos1 = parser.parse(fr);
-		recogerDatosX(datos1);
+		//recogerDatosX(datos1);
 		
 		fr = new FileReader("../../C/integralesV2/prueba2.json");
 		JsonElement datos2 = parser.parse(fr);
-		recogerDatosY(datos2);
+		dataset1 = crearDataset(datos1, datos2);
 		
-		JFreeChart chart = ChartFactory.createLineChart("Titulo", "X", "Y", dataset1);
+		JFreeChart chart = ChartFactory.createScatterPlot("Titulo", "X", "Y", dataset1);
 		ChartPanel panel = new ChartPanel(chart);
 		JFrame ventana = new JFrame("1");
 		ventana.setVisible(true);
@@ -40,28 +42,25 @@ public class Principal {
 		
 	}
 	
-	public static void recogerDatosX (JsonElement datos){
-		int i = 0;
+	public static XYDataset crearDataset (JsonElement datos1, JsonElement datos2){
+		double x ,y;
 		
-		JsonArray array = datos.getAsJsonArray();
-        java.util.Iterator<JsonElement> iter = array.iterator();
-        while (iter.hasNext()) {
-            JsonElement entrada = iter.next();
+		JsonArray array1 = datos1.getAsJsonArray();
+        java.util.Iterator<JsonElement> iter1 = array1.iterator();
+        JsonArray array2 = datos2.getAsJsonArray();
+        java.util.Iterator<JsonElement> iter2 = array2.iterator();
+        
+        while (iter1.hasNext()) {
+            JsonElement entrada = iter1.next();
             JsonPrimitive valor = entrada.getAsJsonPrimitive();
-    		x[i] = valor.getAsDouble();
-    		i++;
+    		//dataset1.setValue(valor.getAsDouble(), "Y", ""+x[i]);
+    		x = valor.getAsDouble();
+    		entrada = iter2.next();
+    		valor = entrada.getAsJsonPrimitive();
+    		y = valor.getAsDouble();
+    		series.add(x,y);
         }
-	}
-	
-	public static void recogerDatosY (JsonElement datos){
-		int i = 0;
-		JsonArray array = datos.getAsJsonArray();
-        java.util.Iterator<JsonElement> iter = array.iterator();
-        while (iter.hasNext()) {
-            JsonElement entrada = iter.next();
-            JsonPrimitive valor = entrada.getAsJsonPrimitive();
-    		dataset1.setValue(valor.getAsDouble(), "Y", ""+x[i]);
-    		i++;
-        }
+        collection.addSeries(series);
+        return collection;
 	}
 }
